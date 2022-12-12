3d9d7e3d279d717329e7f2501896850e7b95e677
Mopholosi <67508554+Mopholosi@users.noreply.github.com>
2022-12-12 14:30:49 +0200

    Operators work

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 const calculatorButtons = document.getElementsByTagName('button');
+const displayScreen = document.getElementById('display-screen');
+let firstNum = null
+    , secondNum = null
+    , operator = null
+    , secondOperand = null
+    , answer = null
 
+// various button events
 const clickButton = () => {
     for (let i = 0; i < calculatorButtons.length; i++) {
         calculatorButtons[i].addEventListener('click', () => {
             if (calculatorButtons[i].classList.contains('number-btn')) {
-                console.log(calculatorButtons[i].innerHTML)
+                storeEnteredNumbers(calculatorButtons[i].innerHTML)
+                updateDisplay();
             } else if (calculatorButtons[i].classList.contains('clear-btn')) {
-                console.log(calculatorButtons[i].innerHTML)
+                clearDisplay();
             } else if (calculatorButtons[i].classList.contains('operator-btn')) {
-                console.log(calculatorButtons[i].innerHTML)
+                storeEnteredOperator(calculatorButtons[i].innerHTML);
+                updateDisplay();
             } else if (calculatorButtons[i].classList.contains('decimal-btn')) {
-                console.log(calculatorButtons[i].innerHTML)
+                console.log(calculatorButtons[i].innerHTML);
             } else if (calculatorButtons[i].classList.contains('sign-btn')) {
-                console.log(calculatorButtons[i].innerHTML)
+                console.log(calculatorButtons[i].innerHTML);
             } else if (calculatorButtons[i].classList.contains('percent-btn')) {
-                console.log(calculatorButtons[i].innerHTML)
+                convertToPercentage(calculatorButtons[i].innerHTML);
             } else if (calculatorButtons[i].classList.contains('equal-btn')) {
-                console.log(calculatorButtons[i].innerHTML)
+                operate(firstNum, secondNum, operator);
+                updateDisplay();
+                firstNum = null;
+                secondNum = null;
+                operator = null;
+                answer = null;
             } else return
         });
     }
@@ -24,18 +38,66 @@ const clickButton = () => {
 
 clickButton();
 
-const operate = (firstNumber, secondNumber, operator) => {
-    if (operator === '+') return firstNumber + secondNumber;
-    else if (operator === '-') return firstNumber - secondNumber;
-    else if (operator === '*') return firstNumber * secondNumber;
-    else if (operator === '/') return firstNumber / secondNumber;
+// store enetered number
+const storeEnteredNumbers = number => {
+    if (firstNum === null && operator === null) firstNum = number;
+    else if (firstNum !== null && operator === null) firstNum += number;
+    else if (firstNum !== null && operator !== null) {
+        if (secondNum === null) secondNum = number;
+        else if (secondNum !== null) secondNum += number;
+        else return;
+    } else return;
+}
+
+// store entered operator
+const storeEnteredOperator = enteredOperator => {
+    if (operator === null) operator = enteredOperator;
     else return;
 }
 
-const updateDisplay = () => {
-    let answer = operate(20, 20, 'plus');
-    document.getElementById('display-screen').textContent = answer;
+// perform the calculation
+const operate = (firstNumber, secondNumber, operator) => {
+    if (operator === '+') answer = Number(firstNumber) + Number(secondNumber);
+    else if (operator === '-') answer = Number(firstNumber) - Number(secondNumber);
+    else if (operator === 'x') answer = Number(firstNumber) * Number(secondNumber);
+    else if (operator === '/') answer = Number(firstNumber) / Number(secondNumber);
+    else return;
 }
 
-const clearDisplay = () =>
-    document.getElementById('display-screen').textContent = "0";
+// update the display screen
+const updateDisplay = () => {
+    if (firstNum === null && operator === null) displayScreen.textContent = 0;
+    else if (firstNum !== null && operator === null) displayScreen.textContent = firstNum;
+    else if (firstNum !== null && operator !== null) {
+        if (secondNum === null) displayScreen.textContent = 0;
+        else if (secondNum !== null) {
+            if (answer === null) displayScreen.textContent = secondNum;
+            else displayScreen.textContent = answer;
+        } else return;
+    } else return;
+    console.log(firstNum, operator, secondNum, answer);
+}
+
+updateDisplay();
+
+// clear the diplay screen ad all saved inputs
+const clearDisplay = () => {
+    displayScreen.textContent = "0";
+    firstNum = null;
+    secondNum = null;
+    answer = null;
+    operator = null;
+    console.log(firstNum, operator, secondNum, answer);
+}
+
+const convertToPercentage = enteredNumber => {
+
+}
+
+const convertToDecimal = enteredNumber => {
+
+}
+
+const convertToNegetiveNumber = enteredNumber => {
+
+}
